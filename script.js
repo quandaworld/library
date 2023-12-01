d112c4c34c6651b06d86c1fd527937b301d34883
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-11-30 21:09:58 -0800

    Add various functions and event listeners - Add functions to add new books to library - Add functions to check if book exists in library - Add functions to get reading status of the added book - Add an event listener to add required attribute to page number if currently reading is checked - Add an event listener to add books when form is submitted

---
+++ script.js
@@ -0,0 +1,69 @@
+const myLibrary = [];
+const title_input = document.getElementById('title');
+const author_input = document.getElementById('author');
+const pages_input = document.getElementById('pages');
+const status_input = document.getElementsByName('status');
+const pageNum_input = document.getElementById('page-number');
+const reading_input = document.getElementById('reading');
+const form = document.querySelector('form');
+const table = document.querySelector('table');
+let read_status = '';
+
+reading_input.addEventListener('click', () => {
+  if (reading_input.checked) pageNum_input.setAttribute('required', '');
+});
+
+form.addEventListener('submit', addBookToLibrary);
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+
+function addBookToLibrary(e) {
+  e.preventDefault();
+  const newBook = new Book(title_input.value, author_input.value, pages_input.value, getStatus());  
+  if (!isInLibrary(newBook)) {
+    myLibrary.push(newBook);
+    displayBook(newBook);
+    form.reset();
+    pageNum_input.removeAttribute('required');
+  } else {
+    alert('This book is already in your library.');
+  }
+}
+
+function displayBook(newBook) {
+  const row = table.insertRow();
+  const title_cell = row.insertCell(0);
+  const author_cell = row.insertCell(1);
+  const pages_cell = row.insertCell(2);
+  const status_cell = row.insertCell(3);
+  const edit_cell = row.insertCell(4);
+  title_cell.innerHTML = newBook.title;
+  author_cell.innerHTML = newBook.author;
+  pages_cell.innerHTML = newBook.pages;
+  status_cell.innerHTML = newBook.status;
+  edit_cell.innerHTML = 'TBU';
+}
+
+function isInLibrary(newBook) {
+  for (const book of myLibrary) {
+    if (book.title.toLowerCase() === newBook.title.toLowerCase()) return true;
+  }
+  return false;
+}
+
+function getStatus() {
+  for (let i = 0; i < status_input.length; i++) {
+    if (status_input[i].checked) {
+      read_status = status_input[i].labels[0].innerText;
+      if (i === 1) read_status += pageNum_input.value;
+    }
+  }
+  return read_status;
+}
+
